4db74bd22a0f343faf161c3413a9e164e9d9befd
maxkung101 <maxkung101@users.noreply.github.com>
2016-04-13 09:41:12 -0400

    Update mysqli_connect.php

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -4,10 +4,10 @@
 // and selects the database.
 
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', 'root');
-DEFINE ('DB_HOST', 'localhost:3306');
-DEFINE ('DB_NAME', 'sitename');
+DEFINE ('DB_USER',     '     '); // Insert your database username between the empty ''.
+DEFINE ('DB_PASSWORD', '     '); // Insert your database password between the empty ''.
+DEFINE ('DB_HOST',     '     '); // Insert your database hostname between the empty ''.
+DEFINE ('DB_NAME',     '     '); // Insert your database name between the empty ''.
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
