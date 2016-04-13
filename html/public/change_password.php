0d65accd88ab75b8ebb1041e04e277a8c0f81c42
maxkung101 <maxkung101@users.noreply.github.com>
2016-04-13 09:44:22 -0400

    Update change_password.php

--- html/public/change_password.php
+++ html/public/change_password.php
@@ -1,4 +1,4 @@
-<?php # Script 18.11 - change_password.php
+<?php # change_password.php
 // This page allows a logged-in user to change their password.
 require ('../includes/config.inc.php'); 
 $page_title = 'Change Your Password';
