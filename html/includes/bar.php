5cf8113fa2adda8feee43391895df5188f4cad0e
maxkung101 <mck7@njit.edu>
2016-04-19 11:02:53 -0400

    update bar.php

--- html/includes/bar.php
+++ html/includes/bar.php
@@ -2,7 +2,8 @@
 // This page completes the HTML template.
 // Display links based upon the login status:
 if (isset($_SESSION['user_id'])) {
-	echo '<a href="../public/logout.php" class="bar" title="Logout"><li>Logout</li></a>
+	echo '<a href="../index.html" class="bar" title="Home Page"><li>Home Page</li></a>
+	<a href="../public/logout.php" class="bar" title="Sign out"><li>Sign out</li></a>
 <a href="../public/change_password.php" class="bar" title="Change Password"><li>Change Password</li></a>
 ';
 	// Add links if the user is an administrator:
@@ -11,9 +12,7 @@ if (isset($_SESSION['user_id'])) {
 		<a href="../admin/view_users.php?associated=0" class="bar" title="View All Users"><li>View All Users</li></a>';
 	}
 } else { //  Not logged in.
-	echo '<a href="../public/register.php" class="bar" title="Register"><li>Register</li></a>
-<a href="../public/login.php" class="bar" title="Login"><li>Login</li></a>
-<a href="../public/forgot_password.php" class="bar" title="Retrieve Password"><li>Retrieve Password</li></a>
-';
+	echo '<a href="../aboutus.html" class="bar" title="About us"><li>About us</li></a>
+	<a href="../contactus.html" class="bar" title="Contact us"><li>Contact us</li></a>';
 }
 ?>
