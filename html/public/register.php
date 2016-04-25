5f570876053e64e5d0d72bf204fa95e2cddfe922
maxkung101 <mck7@njit.edu>
2016-04-25 13:55:33 -0400

    update register.php

--- html/public/register.php
+++ html/public/register.php
@@ -88,12 +88,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 
 				// Send the email:
 				$body = "Thank you for registering at <whatever site>. To activate your account, please click on this link:\n\n";
-				$body .= BASE_URL . 'activate.php?x=' . urlencode($e) . "&y=$a";
-				mail($trimmed['email'], 'Registration Confirmation', $body, 'From: admin@sitename.com');
+				$body .= BASE_URL . 'username_registration/html/public/activate.php?x=' . urlencode($e) . "&y=$a";
+				mail($trimmed['email'], 'Registration Confirmation', $body, 'From: donotreply@sitename.com');
 				
 				// Finish the page:
 				echo '<h3>Thank you for registering! A confirmation email has been sent to your address. Please click on the link in that email in order to activate your account.</h3>';
-				include ('includes/footer.html'); // Include the HTML footer.
+				include ('../includes/footer.html'); // Include the HTML footer.
 				exit(); // Stop the page.
 				
 			} else { // If it did not run OK.
